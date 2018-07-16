489d0ecbdc181a3fa5d30995bea15517df3a82c1
Shantanu V Lad <shantanu.lad@cybergindia.local>
2018-07-16 18:40:02 +0530

    Modified index.js

--- index.js
+++ index.js
@@ -3,12 +3,12 @@
 'use strict';
 
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+const request = require('request');
 
 // Imports dependencies and set up http server
 const
   express = require('express'),
   bodyParser = require('body-parser'),
-  request = require('request'),
   app = express().use(bodyParser.json()); // creates express http server
 
 // Sets server port and logs message on success
